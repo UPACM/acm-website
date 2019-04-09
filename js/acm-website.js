86bbe746ae1b2b97c951174cee48064cd5841660
Menard Ysrael Cruz <ysrael214@gmail.com>
2019-04-09 14:24:41 +0800

    applied single header footer in about

---
+++ js/acm-website.js
@@ -0,0 +1,11 @@
+/* ACM Website Main JS */
+
+function initializeSite(){
+	
+	/* Load Header and Footer Partials */
+	$(".acm-navbar").load("./partials/header.html");
+	$(".acm-footer").load("./partials/footer.html");
+
+}
+
+initializeSite();
